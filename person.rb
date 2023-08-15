ba3cb94ae9b7aabd76dca52dea8ac2bfb965d790
JuanDBta <simplebetsas@gmail.com>
2023-08-15 14:10:07 -0500

    Fix Rubocop offenses

--- person.rb
+++ person.rb
@@ -1,24 +1,23 @@
 class Person
-    attr_reader :id
-    attr_accessor :name, :age, :parent_permission
+  attr_reader :id
+  attr_accessor :name, :age, :parent_permission
 
-    def initialize(name ="Unknown", age, parent_permission = true)    
-        @id = Random.rand(1..1000)
-        @name = name
-        @age = age
-        @parent_permission = parent_permission  
-        end
-        
-    def of_age?
-        if @age >= 18
-            true
-        else false
-        end
-    end
+  def initialize(age, name: 'Unknown', parent_permission: true)
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age.to_i
+    @parent_permission = parent_permission
+  end
 
-def can_use_services?
-  if @of_age || @parent_permission
-    true
-  else false
+  private
+
+  def of_age?
+    @age >= 18
+  end
+
+  public
+
+  def can_use_services?
+    of_age? || @parent_permission
   end
 end
--- student.rb
+++ student.rb
@@ -1,14 +1,15 @@
-require "./person"
+require './person'
 
+# Creating class Student
 class Student < Person
-    def initialize (classroom, name = "Unknown", age, parent_permission = true)
-        super(name, age, parent_permission)
-        @classromm = classroom
-    end
-
+  def initialize(age, classroom, name: 'Unknown', parent_permission: true)
     super(name, age, parent_permission)
+    @classromm = classroom
+  end
 
-    def play_hooky
-        "¯\(ツ)/¯"
-    end
+  super(name, age, parent_permission)
+
+  def play_hooky
+    '¯(ツ)/¯'
+  end
 end
